97e1034de3c0f8ebfb499eff6b8e72e6f441077f
Jess Dieuique <jessica.dieuique@bench.co>
2020-03-29 18:54:11 -0700

    Reset timeNow upon test completion

--- user/service_test.go
+++ user/service_test.go
@@ -7,41 +7,41 @@ import (
 	"testing"
 )
 
-type SpyStore struct {
+type SpyDataSource struct {
 	called bool
 	preferredTime int
 }
 
-func (s *SpyStore) Create(ctx context.Context, preferredTime int) (string, error) {
+func (s *SpyDataSource) Create(ctx context.Context, preferredTime int) (string, error) {
 	s.called = true
 	s.preferredTime = preferredTime
 	return "1", nil
 }
 
-func (s *SpyStore) SetPreferredTime(ctx context.Context, id string, preferredTime int) error {
+func (s *SpyDataSource) SetPreferredTime(ctx context.Context, id string, preferredTime int) error {
 	s.called = true
 	s.preferredTime = preferredTime
 	return nil
 }
 
 func TestCreateNewUser(t *testing.T) {
-	var userStore = &SpyStore{ false, 0 }
+	var dataSource = &SpyDataSource{ false, 0 }
 
 	ctx := context.Background()
-	entity := UserEntity{ userStore }
+	entity := UserEntity{ dataSource }
 	_, err := entity.CreateNewUser(ctx)
 
 	if err != nil {
-		t.Errorf(fmt.Sprintf("Error creating user"))
+		t.Errorf(fmt.Sprintf("Error creating new user"))
 	}
 
-	if userStore.called != true {
-		t.Errorf(fmt.Sprintf("UserStore was not called with create"))
+	if dataSource.called != true {
+		t.Errorf(fmt.Sprintf("CreateNewUser did not call data source"))
 	}
 }
 
 func TestCreateNewUserPreferredTime(t *testing.T) {
-	var userStore = &SpyStore{ false, 0 }
+	var dataSource = &SpyDataSource{ false, 0 }
 
 	hour := 6
 	min := 15
@@ -50,17 +50,21 @@ func TestCreateNewUserPreferredTime(t *testing.T) {
 		return t
 	}
 
+	defer func() {
+		timeNow = time.Now
+	}()
+
 	ctx := context.Background()
-	entity := UserEntity{ userStore }
+	entity := UserEntity{ dataSource }
 	_, err := entity.CreateNewUser(ctx)
 
 	if err != nil {
-		t.Errorf(fmt.Sprintf("Error creating user"))
+		t.Errorf(fmt.Sprintf("Error creating new user"))
 	}
 
 	preferredTime := hour * 60 + min
 
-	if userStore.preferredTime != preferredTime {
-		t.Errorf(fmt.Sprintf("Error creating preferred time %d, not %d", userStore.preferredTime, preferredTime))
+	if dataSource.preferredTime != preferredTime {
+		t.Errorf(fmt.Sprintf("Error creating user with preferred time %d", dataSource.preferredTime))
 	}
 }
