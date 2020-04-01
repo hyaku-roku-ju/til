a0d76e958016126f134dde5b8e7c00c64a391fcf
Lainera <fistlain@hotmail.com>
2020-03-31 22:48:23 -0700

    Add update preference for mongo data source

--- mad/mongo-adapter.go
+++ mad/mongo-adapter.go
@@ -3,43 +3,73 @@ package mad
 import (
 	"context"
 	"fmt"
-  "github.com/hyaku-roku-ju/til/user"
+	"github.com/hyaku-roku-ju/til/user"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type UserDataSource struct {
-  Db *mongo.Database 
+	Db *mongo.Database
+}
+
+func (self *UserDataSource) SetPreferredTime(ctx context.Context, id string, preferredTime user.PreferredTime) error {
+	collection := self.Db.Collection("users")
+	fail := make(chan error)
+
+	go func() {
+		id, err := primitive.ObjectIDFromHex(id)
+		if err != nil {
+			fail <- err
+		}
+		_, err = collection.UpdateOne(
+			ctx,
+			bson.M{"_id": id},
+			bson.D{{"$set",	bson.M{"preferredTime": bson.M{
+					"hour": preferredTime.Hour,
+					"min":  preferredTime.Min,
+				}}}},
+		)
+		if err != nil {
+			fail <- err
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case err := <-fail:
+		return err
+	}
 }
 
 func (self *UserDataSource) Create(ctx context.Context, preferredTime user.PreferredTime) (string, error) {
-  collection := self.Db.Collection("users")
-  fail := make(chan error)
-  success := make(chan string)
-  
-  go func() {
-    result, err := collection.InsertOne(ctx, bson.M{"preferredTime": bson.M{
-      "hour": preferredTime.Hour,
-      "min": preferredTime.Min,
-    }})
+	collection := self.Db.Collection("users")
+	fail := make(chan error)
+	success := make(chan string)
 
-    if err != nil {
-      fail<-err
-    }
-    objectId, ok := result.InsertedID.(primitive.ObjectID)
-    if !ok {
-       fail<- fmt.Errorf("Failed to cast insertOne result to ObjectID") 
-    }
-    success<-objectId.Hex()
-  }()
- 
-  select {
-    case <-ctx.Done():
-      return "", ctx.Err()
-    case err := <-fail:
-      return "", err
-    case id := <-success:
-      return id, nil
-  }
+	go func() {
+		result, err := collection.InsertOne(ctx, bson.M{"preferredTime": bson.M{
+			"hour": preferredTime.Hour,
+			"min":  preferredTime.Min,
+		}})
+
+		if err != nil {
+			fail <- err
+		}
+		objectId, ok := result.InsertedID.(primitive.ObjectID)
+		if !ok {
+			fail <- fmt.Errorf("Failed to cast insertOne result to ObjectID")
+		}
+		success <- objectId.Hex()
+	}()
+
+	select {
+	case <-ctx.Done():
+		return "", ctx.Err()
+	case err := <-fail:
+		return "", err
+	case id := <-success:
+		return id, nil
+	}
 }
