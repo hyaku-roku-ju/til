690452f81a8f08431f74beb6f786ebbe9a45f848
Jess Dieuique <jessica.dieuique@bench.co>
2020-03-31 22:11:40 -0700

    A short description about TIL bot

--- user/service.go
+++ user/service.go
@@ -9,7 +9,7 @@ type UserEntity struct {
 	dataSource UserDataSource
 }
 
-// Create timeNow for testing
+// Set time.Now outside of method for testing
 var timeNow = time.Now
 
 func (u UserEntity) CreateNewUser(ctx context.Context) (string, error) {
