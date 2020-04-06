5834e90f17ebcca7ec6a1eacd1ea0d4daeb2108a
Jess Dieuique <jessica.dieuique@bench.co>
2020-04-05 21:16:46 -0700

    add create and get methods for telegram service

--- telegram/service.go
+++ telegram/service.go
@@ -6,3 +6,21 @@ type TelegramRepository interface {
 	Create(ctx context.Context, userId string, telegramId string) error
 	GetIdByUserId(ctx context.Context, userId string) (string, error)
 }
+
+type TelegramService struct {
+	Repo TelegramRepository
+}
+
+func NewService(repo TelegramRepository) TelegramService {
+  return TelegramService{repo}
+}
+
+func (t *TelegramService) StoreTelegramId(ctx context.Context, userId string, telegramId string) error {
+	err := t.Repo.Create(ctx, userId, telegramId)
+	return err
+}
+
+func (t *TelegramService) GetIdByUserId(ctx context.Context, userId string) (string, error) {
+	id, err := t.Repo.GetIdByUserId(ctx, userId)
+	return id, err
+}
