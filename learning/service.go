dd8e59ae3d1c12808454342ece28508abf7a7dd8
Lainera <fistlain@hotmail.com>
2020-04-13 20:43:04 -0700

    Add random learning method to repo interface

--- learning/service.go
+++ learning/service.go
@@ -6,6 +6,7 @@ import (
 
 type LearningRepository interface {
   StoreLearning(ctx context.Context, learning Learning) (string, error)
+  GetRandomLearning(ctx context.Context, reporterId string) (Learning, error)
 }
 
 type LearningService struct {
@@ -14,6 +15,28 @@ type LearningService struct {
 
 func NewService(repo LearningRepository) LearningService {
   return LearningService{repo}
+}
+
+func (self *LearningService) GetRandomLearning(ctx context.Context, reporterId string) (Learning, error) {
+  complete := make(chan Learning)
+  fail := make(chan error)
+
+  go func() {
+    learning, err := self.learningRepository.GetRandomLearning(ctx, reporterId)
+    if err != nil {
+      fail<-err
+    }
+    complete<-learning
+  }()
+
+  select {
+    case <-ctx.Done():
+      return Learning{}, ctx.Err()
+    case err := <-fail:
+      return Learning{}, err
+    case learning := <-complete:
+      return learning, nil
+  }
 }
 
 func (self *LearningService) StoreLearning(ctx context.Context, learning Learning) (learningId string, err error) {
